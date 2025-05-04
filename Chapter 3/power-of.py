0b4e90b88495883b7ea52fa08bc6f1ce0ed626af
glovek08 <gbrielbbarn@gmail.com>
2025-05-04 18:22:32 -0300

    reload modules

--- Chapter 3/power-of.py
+++ Chapter 3/power-of.py
@@ -4,7 +4,7 @@ def to_power(base, exponent):
         result = result[:10]+'e+'+str((len(result) - 1));
     return (result);
 
-print("The result of 2^100 is: "+"{:.2f}".format(2 ** 100)[:5]+"e+26");
+print("The result of 2^100 is: "+"{:.2f}".format(2 ** 100)[:5]+"e+30");
 num = 5
 exp = 100
 print(f'The result of {num}^{exp} is: {str(to_power(num, exp))}');
---
+++ Chapter 3/reload_module.py
@@ -0,0 +1,8 @@
+import importlib
+import script1
+
+print(script1.x);
+script1.x = "Hello";
+print('x before reload: '+ script1.x +'\n');
+importlib.reload(script1);
+print('x after reload: '+ script1.x +'\n');
